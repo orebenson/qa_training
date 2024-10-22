674a9eb1b999df671cc8c98b06b148745a23df5c
orebenson <orebenson@hotmail.com>
2024-10-22 11:28:52 +0100

    Updated files

--- qa-SQL/answers.sql
+++ qa-SQL/answers.sql
@@ -72,21 +72,33 @@ select country.Name, city.Population from country join city on city.CountryCode 
 --     9. Using aggregate functions, return the number of countries the database contains.
 select count(distinct CountryCode) as distrinct_countries from (select CountryCode from city union select Code as CountryCode from country union select CountryCode from city) as combined;
 --     10. What are the top ten largest countries by area?
-
+select Name from country order by SurfaceArea desc limit 10;
 --     11. List the five largest cities by population in Japan.
+select city.Name, city.Population from city join country on city.CountryCode = country.Code where country.Name = 'Japan' order by city.Population desc limit 5;
 --     12. List the names and country codes of every country with Elizabeth II as its Head of State. You will need to fix the mistake first!
+update country set country.HeadOfState = 'Elizabeth II' where country.HeadOfState like '%Elisabeth%';
+select Name, Code from country where HeadOfState like '%Elizabeth II%';
 --     13. List the top ten countries with the smallest population-to-area ratio. Discard any countries with a ratio of 0.
+select (Population / SurfaceArea) as ratio from country where (Population / SurfaceArea) > 0 order by (Population / SurfaceArea) limit 10;
 --     14. List every unique world language.
+select distinct Language from countryLanguage;
 --     15. List the names and GNP of the world's top 10 richest countries.
+select Name, GNP from country order by GNP desc limit 10;
 --     16. List the names of, and number of languages spoken by, the top ten most multilingual countries.
+select country.Name, count(countrylanguage.Language) from country join countrylanguage on country.Code = countrylanguage.CountryCode group by country.Name order by count(countrylanguage.Language) desc limit 10;
 --     17. List every country where over 50% of its population can speak German.
+select country.Name, countrylanguage.Percentage from country join countrylanguage on country.Code = countrylanguage.CountryCode where countrylanguage.Language = 'German' and countrylanguage.Percentage > 50;
 --     18. Which country has the worst life expectancy? Discard zero or null values.
+select Name, LifeExpectancy from country where LifeExpectancy is not null and LifeExpectancy > 0 order by LifeExpectancy limit 1;
 --     19. List the top three most common government forms.
+select distinct GovernmentForm, count(GovernmentForm) from country group by GovernmentForm order by count(GovernmentForm) desc limit 3;
 --     20. How many countries have gained independence since records began?
+select count(*) from country where IndepYear is not null;
 
 -- Movielens questions:
--- use movielens;
+use movielens;
 --     1. List the titles and release dates of movies released between 1983-1993 in reverse chronological order.
+select title, release_date from movies where release_date > 1982 and release_date < 1994 order by release_date desc;
 --     2. Without using LIMIT, list the titles of the movies with the lowest average rating.
 --     3. List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings.
 --     4. List the unique titles of each of the movies released on the most popular release day.
